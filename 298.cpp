775cba5c3d5761cfdaba7358c0afb3ad4c97e6fb
birdmanwings <747458025@qq.com>
2019-09-24 00:29:56 +0800

    2019.9.24 1题

---
+++ 298.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int longestConsecutive(TreeNode* root) {
+
+    }
+};
+
+int main() {
+    string A = "1807", B = "7810";
+    Solution S;
+    cout << S.getHint(A, B) << endl;
+    return 0;
+}
