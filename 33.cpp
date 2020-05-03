3f866609fe96e055a7a69b14e9196b7559c9ccfa
birdmanwings <747458025@qq.com>
2020-05-03 15:48:09 +0800

    第二轮：33，34

--- 33.cpp
+++ 33.cpp
@@ -4,6 +4,23 @@
 
 using namespace std;
 
+/*
+ * 观察所有的旋转数组
+ * 0　　1　　2　　4　　5　　6　　7
+ * 7　　0　　1　　2　　4　　5　　6
+ * 6　　7　　0　　1　　2　　4　　5
+ * 5　　6　　7　　0　　1　　2　　4
+ * 4　　5　　6　　7　　0　　1　　2
+ * 2　　4　　5　　6　　7　　0　　1
+ * 1　　2　　4　　5　　6　　7　　0
+ * 因为是要求O(log^n)，肯定是二分，那么二分的关键就在于，我们找到mid点的时候，需要判断target的位置是属于左边还是右边
+ * 然后我们观察数组能够发现，当中间值处于右边的升序队列时候，我们可以发现当mid值大于最右边的值的时候，那么mid右边的数是有序的
+ * 当小于的时候，则左边是有序的，那么我们就可以先得出哪一部分是有序的，然后才能判断target是否在这个有序序列中，然后就能修正left或者right
+ * 从而不断折叠二分。
+ * 总结下，我们做类似这种题目的时候可以先把所有例子举出来，来便于我们观察规律，然后要剖析到关键点。
+ * 时间复杂度O(logn)
+ * 空间复杂度O(1)
+ */
 class Solution {
 public:
     int search(vector<int> &nums, int target) {
@@ -12,7 +29,7 @@ public:
             int mid = left + (right - left) / 2;
             if (nums[mid] == target) return mid;
             else if (nums[mid] < nums[right]) {
-                if (nums[mid] < target && nums[right] >= target) left = mid + 1;
+                if (nums[mid] < target && nums[right] >= target) left = mid + 1; // 注意下=边界情况
                 else right = mid - 1;
             } else {
                 if (nums[left] <= target && nums[mid] > target) right = mid - 1;
@@ -22,6 +39,8 @@ public:
         return -1;
     }
 };
+
+
 
 int main() {
 
--- 34.cpp
+++ 34.cpp
@@ -4,63 +4,66 @@
 
 using namespace std;
 
+/*
+ * 这篇文章把二分搜索讲的很清除https://leetcode-cn.com/problems/find-first-and-last-position-of-element-in-sorted-array/solution/er-fen-cha-zhao-suan-fa-xi-jie-xiang-jie-by-labula/
+ * 时间复杂度O(logn)
+ * 空间复杂度O(1)
+ */
 class Solution {
-private:
-    int findLowerBound(vector<int> &nums, int target) {
-        int size = nums.size();
-        int left = 0;
-        int right = size - 1;
-        while (left < right) {
-            int mid = (left + right) >> 1;
-            if (nums[mid] < target) {
-                left = mid + 1;
-            } else {
-                right = mid;
-            }
-        }
-        if (nums[left] != target) {
-            return -1;
-        }
-        return left;
+ private:
+  int findLowerBound(vector<int> &nums, int target) {
+    int left = 0, right = nums.size() - 1;
+    while (left <= right) {
+      int mid = left + (right - left) / 2;
+      if (nums[mid] < target) {
+        left = mid + 1;
+      } else if (nums[mid] > target) {
+        right = mid - 1;
+      } else if (nums[mid] == target) { // 关键在这里，我们找到target值时候需要先缩小右边界，才能保证返回小的下标
+        right = mid - 1;
+      }
     }
-
-    int findUpBound(vector<int> &nums, int target) {
-        int size = nums.size();
-        int left = 0;
-        int right = size - 1;
-        while (left < right) {
-            int mid = (left + right + 1) >> 1;
-            if (nums[mid] > target) {
-                right = mid - 1;
-            } else {
-                left = mid;
-
-            }
-        }
-        if (nums[left] != target) {
-            return -1;
-        }
-        return left;
+    if (left >= nums.size() || nums[left] != target) { // 注意边界情况，如果target过大的时候,left可能存在越界的情况，其他情况是不存在target
+      return -1;
     }
+    return left;
+  }
 
-
-public:
-    vector<int> searchRange(vector<int> &nums, int target) {
-        int size = nums.size();
-        if (size == 0) {
-            return {-1, -1};
-        }
-        int num1 = findLowerBound(nums, target);
-
-        if (num1 == -1) {
-            return {-1, -1};
-        }
-        int num2 = findUpBound(nums, target);
-        return {num1, num2};
+  int findUpBound(vector<int> &nums, int target) {
+    int left = 0, right = nums.size() - 1;
+    while (left <= right) {
+      int mid = left + (right - left) / 2;
+      if (nums[mid] < target) {
+        left = mid + 1;
+      } else if (nums[mid] > target) {
+        right = mid - 1;
+      } else if (nums[mid] == target) {
+        left = mid + 1;
+      }
     }
+    if (right < 0 || nums[right] != target) { // 同样注意边界情况
+      return -1;
+    }
+    return right;  // 其实这里返回left和right都一样因为跳出条件为left==right的时候
+  }
+
+ public:
+  vector<int> searchRange(vector<int> &nums, int target) {
+    int size = nums.size();
+    if (size == 0) {
+      return {-1, -1};
+    }
+    int num1 = findLowerBound(nums, target);
+
+    if (num1 == -1) {
+      return {-1, -1};
+    }
+    int num2 = findUpBound(nums, target);
+    return {num1, num2};
+  }
 };
 
 int main() {
 
-    return 0;
+  return 0;
 }
